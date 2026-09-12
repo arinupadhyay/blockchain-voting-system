7f0b4f0c14699fb7c03a707872fe812f5e07172b
randhir0003 <rpra1616@gmail.com>
2026-09-12 18:21:26 +0530

    Mini Voting System

---
+++ vote.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "========================================" << endl;
+    cout << "        STUDENT ELECTION SYSTEM         " << endl;
+    cout << "========================================" << endl;
+
+    cout << endl;
+
+    cout << "Election ID      : E001" << endl;
+    cout << "Election Name    : Student Council 2026" << endl;
+    cout << "Election Status  : NOT STARTED" << endl;
+
+    cout << endl;
+
+    cout << "--------------- CANDIDATES -------------" << endl;
+    cout << "1. Randhir Pratap Singh" << endl;
+    cout << "2. Rahul Sharma" << endl;
+    cout << "3. Aman Verma" << endl;
+
+    cout << endl;
+
+    cout << "-----------------------------------------" << endl;
+    cout << "        Voting will begin soon...        " << endl;
+    cout << "-----------------------------------------" << endl;
+
+    return 0;
+}
